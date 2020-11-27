78993992f23aaad3e8be0fd3aba984a8430c6e5c
Lthiellaud <laurence.thiellaud@gmail.com>
2020-11-27 11:26:40 +0100

    Add of the class WriteSymptomCountToFile to write the result.out file

--- Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
@@ -8,7 +8,7 @@ public class AnalyticsCounter {
 
 	public static void main(String[] args) throws Exception {
 		// Definition of the input and output files
-		String symptomReadFile = ".\\Project02Eclipse\\symptoms0.txt";
+		String symptomReadFile = ".\\Project02Eclipse\\symptoms.txt";
 		String symptomCounterFile = ".\\Project02Eclipse\\result.out";
 
 		// CreatE a ArrayList containing all the lines of th input file
@@ -17,14 +17,11 @@ public class AnalyticsCounter {
 
 		// Sort and count the symptom
 		SymptomCounter symptomCounter = new SymptomCounter();
-		TreeMap<String, Integer> countedSymptom = symptomCounter.countSymptom(symptomList);
+		TreeMap<String, Integer> countedAndSortedSymptom = symptomCounter.countSymptom(symptomList);
 
 		// next generate output
-		FileWriter writer = new FileWriter (symptomCounterFile);
-		for (String symptom : countedSymptom.keySet()) {
-			writer.write(symptom+"="+countedSymptom.get(symptom)+"\n");
-		}
+		WriteSymptomCountToFile writeSymptom = new WriteSymptomCountToFile(symptomCounterFile);
+		writeSymptom.outSymptom(countedAndSortedSymptom);
 
-		writer.close();
 	}
 }
---
+++ Project02Eclipse/src/com/hemebiotech/analytics/WriteSymptomCountToFile.java
@@ -0,0 +1,36 @@
+package com.hemebiotech.analytics;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.TreeMap;
+
+/**
+ * Simple brute force implementation
+ *
+ */
+public class WriteSymptomCountToFile {
+    private String filepath;
+    /**
+     *
+     * @param filepath a full or partial path to the result file
+     */
+    WriteSymptomCountToFile(String filepath) {
+        this.filepath=filepath;
+    }
+    /**
+     * Write in the result file the content of a TreeMap in the format "key=value" - one key per line.
+     */
+    public void outSymptom(TreeMap<String, Integer> countedAndSortedSymptom) {
+        if (this.filepath != null) {
+            try {
+                FileWriter writer = new FileWriter(this.filepath);
+                for (String symptom : countedAndSortedSymptom.keySet()) {
+                    writer.write(symptom + "=" + countedAndSortedSymptom.get(symptom) + "\n");
+                }
+                writer.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
